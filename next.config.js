1023ae6d6e04a85a4b4b11a9a4530653e551e19b
cubedhuang <99danielh@gmail.com>
2021-07-20 21:02:27 -0400

    add support link

--- next.config.js
+++ next.config.js
@@ -8,6 +8,11 @@ module.exports = {
 				destination:
 					"https://discord.com/oauth2/authorize?client_id=785196430796914758&permissions=339078215&scope=bot%20applications.commands",
 				permanent: true
+			},
+			{
+				source: "/support",
+				destination: "https://discord.gg/H4bndawejj",
+				permanent: true
 			}
 		];
 	}
