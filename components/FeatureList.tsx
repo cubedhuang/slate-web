f39b21ceadc5d3ffde9219da641db7836ebcc0d3
cubedhuang <99danielh@gmail.com>
2021-11-17 16:22:20 -0500

    update to slashes

--- components/FeatureList.tsx
+++ components/FeatureList.tsx
@@ -68,40 +68,52 @@ export default function FeatureList() {
 
 				<div className="list">
 					<Feature name="YouTube Together" color="#e84118" order={1}>
-						YouTube Together is part of a new set of Discord features called
-						Activities that let you interact with others in voice channels.
-						Start a YouTube Together session with <code>;activity</code>!
+						YouTube Together is part of a new set of Discord
+						features called Activities that let you interact with
+						others in voice channels. Start a YouTube Together
+						session with <code>/activity</code>!
 					</Feature>
 					<Feature name="Ghost Ping Logger" color="#487eb0" order={2}>
-						Slate has a versatile logging system that lets you track down ghost
-						pings alongside potentially obscene statuses, nickname changes, and
-						members leaving!
+						Slate has a versatile logging system that lets you track
+						down ghost pings alongside potentially obscene statuses,
+						nickname changes, and members leaving!
 					</Feature>
 					<Feature name="Music Player" color="#8c7ae6" order={3}>
-						Slate has an advanced music player for voice channels, including
-						song looping, volume controls, loading Spotify playlists, and more!
+						Slate has an advanced music player for voice channels,
+						including song looping, volume controls, loading Spotify
+						playlists, and more!
 					</Feature>
 					<Feature name="Leveling System" color="#192a56" order={4}>
 						As people talk, they&apos;ll gain experience. Use the{" "}
-						<code>;lb</code> and <code>;rank</code> commands to measure how
-						active people are compared to each other in your servers!
+						<code>/leaderboard xp</code> and{" "}
+						<code>/leaderboard rank</code> commands to measure how
+						active people are compared to each other in your
+						servers!
 					</Feature>
 					<Feature name="Complex Math" color="#438a20" order={5}>
-						Use our powerful <code>;math</code> command to evaluate complex math
-						expressions, equations, and systems. Factoring quadratics? Just{" "}
-						<code>;math factor x^2-4</code>! Integrating functions? Use{" "}
-						<code>;math integrate 2x</code>!
+						Use our powerful <code>/math</code> command to evaluate
+						complex math expressions, equations, and systems.
+						Factoring quadratics? Just{" "}
+						<code>/math factor x^2-4</code>! Integrating functions?
+						Use <code>/math integrate 2x</code>!
 					</Feature>
 					<Feature name="Advanced Info" color="#273c75" order={6}>
 						Slate has several comprehensive info commands like{" "}
-						<code>;userinfo</code> that give you as much information as
-						possible, including username and nickname history!
+						<code>/info user</code> that give you as much
+						information as possible, including username and nickname
+						history!
 					</Feature>
-					<Feature name="&hellip;And More!" color="#2f3640" order={7} noHover>
+					<Feature
+						name="&hellip;And More!"
+						color="#2f3640"
+						order={7}
+						noHover
+					>
 						<div className="invite">
 							<div>
-								For a full list of features, invite Slate to your server and use
-								the <code>;help</code> command!
+								For a full list of features, invite Slate to
+								your server and use the <code>;help</code>{" "}
+								command!
 							</div>
 							<span className="link">
 								<a
