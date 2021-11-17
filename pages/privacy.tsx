63113d889b5acc90cc0f1ed325940689c6fc3da8
cubedhuang <99danielh@gmail.com>
2021-11-17 15:53:29 -0500

    update privacy policy

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -8,7 +8,10 @@ export default function Privacy() {
 				<meta name="description" content="Slate's privacy policy." />
 
 				<meta property="og:title" content="Slate – Privacy Policy" />
-				<meta property="og:description" content="Slate's privacy policy." />
+				<meta
+					property="og:description"
+					content="Slate's privacy policy."
+				/>
 			</Head>
 
 			<style jsx>
@@ -23,8 +26,13 @@ export default function Privacy() {
 							padding-top: 2em;
 						}
 
-						h2 {
+						h2,
+						h3 {
 							padding-top: 0.5em;
+						}
+
+						h3 {
+							font-size: 1.2rem;
 						}
 
 						p {
@@ -42,51 +50,56 @@ export default function Privacy() {
 				<h1>Privacy Policy</h1>
 
 				<p>
-					All of the following applies to information collected with the Discord
-					application &quot;Slate#4283&quot; (785196430796914758) through the
-					Discord bot gateway.
+					All of the following applies to information collected with
+					the Discord application &quot;Slate#4283&quot;
+					(785196430796914758) through the Discord bot gateway.
 				</p>
 
 				<h2>What information do we store and why?</h2>
 
 				<p>
-					We store our users&apos; IDs for all of the following information. All
-					of this information can be accessed by people sharing a mutual server
-					with Slate.
+					We store our users&apos; IDs for all of the following
+					information. All of this information can be accessed by
+					people sharing a mutual server with Slate.
 				</p>
 
 				<ol>
 					<li>
-						We store user IDs and the number of times the commands have been run
-						for the &quot;fun&quot; commands like <code>;count</code> and{" "}
-						<code>;multiply</code>.
+						We store user IDs and the number of times the commands
+						have been run for the &quot;fun&quot; commands like{" "}
+						<code>count</code> and <code>multiply</code>.
 					</li>
 					<li>
-						We store past usernames and nicknames associated with specific user
-						IDs. This is used for the <code>;userinfo</code> command and
-						moderation purposes.
+						We store past usernames and nicknames associated with
+						specific user IDs. This is used for the{" "}
+						<code>/info user</code> command and moderation purposes.
 					</li>
 					<li>
-						We store user IDs in order to keep user experience for the{" "}
-						<code>;rank</code> and <code>;leaderboard</code> commands.
+						We store user IDs in order to keep user XP for the
+						messaging leaderboard and the <code>/leaderboard</code>
+						command.
 					</li>
 					<li>
 						We store server IDs in order to moderate and maintain
-						server-specific settings. This includes, but is not necessarily
-						limited to custom prefixes, disabled channels, logging channels, and
+						server-specific settings. This includes custom command
+						prefixes, disabled channels, logging channels, and
 						logging toggles.
 					</li>
-					<li>
-						We observe (but don't store) presence changes in order to facilitate
-						logs for possibly obscene statuses.
-					</li>
 				</ol>
+
+				<h3>What about logging?</h3>
+
+				<p>
+					We observe, scan, but <u>don't store</u> any information
+					that Slate logs regarding messages, presence changes, or
+					nickname changes.
+				</p>
 
 				<h2>Third Party Data Sharing</h2>
 
 				<p>
-					We share data about the number of commands that are run with Statcord.
-					All of the shared information is anonymous.
+					We share data about the number of commands that are run with
+					Statcord. All shared information is anonymous.
 				</p>
 
 				<p>
@@ -104,9 +117,9 @@ export default function Privacy() {
 				<h2>Deleting Data</h2>
 
 				<p>
-					If you wish to delete data from Slate, use the <code>;delete</code>{" "}
-					command. This will allow you to select the category of data that you
-					wish to delete.
+					If you wish to delete data from Slate, use the{" "}
+					<code>/delete</code> command. This will allow you to select
+					the category of data that you wish to delete.
 				</p>
 
 				<h2>Questions and Concerns</h2>
@@ -123,7 +136,7 @@ export default function Privacy() {
 					.
 				</p>
 
-				<p>This policy was last updated July 20th, 2021.</p>
+				<p>This policy was last updated November 17th, 2021.</p>
 			</main>
 		</>
 	);
